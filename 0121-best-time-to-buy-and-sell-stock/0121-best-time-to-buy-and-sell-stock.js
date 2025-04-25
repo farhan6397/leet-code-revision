8a53ab41d5d34cd8f0ab3a3f46d7cef2a1334869
Farhan <151362771+farhan6397@users.noreply.github.com>
2025-04-26 02:36:10 +0530

    Time: 0 ms (100%), Space: 65 MB (65.65%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxprofit = 0;
+    let min = prices[0];
+    for(let i=0; i<prices.length; i++){
+        if(prices[i]<min){
+            min = prices[i];
+
+        }
+        let profit = prices[i]-min;
+        maxprofit = Math.max(maxprofit, profit);
+    
+    }
+    return maxprofit;
+}
